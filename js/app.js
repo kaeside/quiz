1b843071abf48f3a096604b462d4201519e5d30e
scottdiallo <diallot@me.com>
2016-04-15 11:26:26 -0700

    quiz questions created in js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+var quiz = [{
+        question: "What team play in New York",
+        choices:["knicks","Lakers", "Bulls"],
+        answer:0
+    }, {
+        question: "What team play in Charlotte",
+        choices:["Pelicans","Hornets", "Bobcats"],
+        answer:1
+    }, {
+        question: "What team play in Oakland",
+        choices:["knicks","Warriors", "Pistons"],
+        answer:1
+    }
+];
 
-
-$(function () {
-  console.log("hello");
+$(function() {
+    console.log("hello");
 });
 // Make an array
 //Should have objects within the array

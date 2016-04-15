d9727f0dc97dfebec99be89f6869e06583da9b7b
scottdiallo <diallot@me.com>
2016-04-15 13:23:50 -0700

    all questions generated

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
 var currentQuestionIndex = 0;
 var quiz = [{
-        question: "What team plays in New York?",
-        choices: ["knicks", "Lakers", "Bulls"],
-        answer: 0
-    }, {
-        question: "What teams play in Charlotte?",
-        choices: ["Pelicans", "Hornets", "Bobcats"],
-        answer: 1
-    }, {
-        question: "What teams play in Oakland?",
-        choices: ["knicks", "Warriors", "Pistons"],
-        answer: 1
-    }, {
-        question: "What teams play in Phoenix?",
-        choices: ["Thunder", "Spurs", "Suns"],
-        answer: 2
-    }];
+    question: "What team plays in New York?",
+    choices: ["knicks", "Lakers", "Bulls"],
+    answer: 0
+}, {
+    question: "What teams play in Charlotte?",
+    choices: ["Pelicans", "Hornets", "Bobcats"],
+    answer: 1
+}, {
+    question: "What teams play in Oakland?",
+    choices: ["knicks", "Warriors", "Pistons"],
+    answer: 1
+}, {
+    question: "What teams play in Phoenix?",
+    choices: ["Thunder", "Spurs", "Suns"],
+    answer: 2
+}];
 
-    function buildQuestion(num){
-      $('.question').text(quiz[num].question);
-      for (var i = 0; i < quiz[num].choices.length; i++) {
+function buildQuestion(num) {
+    $('.question').text(quiz[num].question);
+    for (var i = 0; i < quiz[num].choices.length; i++) {
         $('.allChoices').append("<p>" + quiz[num].choices[i] + "</p>");
-      }
-    };
+    }
+
+}
 
 $(function() {
     console.log("hello");
@@ -30,11 +31,18 @@ $(function() {
     buildQuestion(currentQuestionIndex);
     currentQuestionIndex++;
 
-    $('#nextButton').click(function(){
-      $(".allChoices").empty();
-      buildQuestion(currentQuestionIndex);
-      currentQuestionIndex++;
-    })
+    $('#nextButton').click(function() {
+        $(".allChoices").empty();
+        buildQuestion(currentQuestionIndex);
+        currentQuestionIndex++;
+        if (currentQuestionIndex === 4) {
+            $('#nextButton').hide();
+            alert('done');
+        }
+    });
+    $(".allChoices").click(function(){
+        alert(choices);
+    });
 
 });
 

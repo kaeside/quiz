8e9f7a7d42340b60ca0107eb5bf493e284ee90ea
Kaeside Iwuagwu <kaeside.iwuagwu@gmail.com>
2016-04-15 14:00:21 -0400

    added comments to js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log("test");
+
+
+$(function () {
+  console.log("hello");
+});
+// Make an array
+//Should have objects within the array
+// The objects should contain of the questions, multiple choices, actual answer

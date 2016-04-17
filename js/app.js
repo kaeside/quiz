39b50359356a66972da581040298fdebf18085e0
scottdiallo <diallot@me.com>
2016-04-17 08:31:03 -0700

    bug fixed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+// Globar variables
 var currentQuestionIndex = 0;
+
 var quiz = [{
     question: "What team plays in New York?",
     choices: ["knicks", "Lakers", "Bulls"],
@@ -29,33 +31,49 @@ $(function() {
     console.log("hello");
 
     buildQuestion(currentQuestionIndex);
-    currentQuestionIndex++;
-
     $('#nextButton').click(function() {
         $(".allChoices").empty();
-        buildQuestion(currentQuestionIndex);
         currentQuestionIndex++;
         if (currentQuestionIndex === 4) {
             $('#nextButton').hide();
             alert('done');
+            // show user results
+            $('results').click(function(){
+
+            });
+        }
+        // start a new game
+        else {
+            buildQuestion(currentQuestionIndex);
         }
     });
     $(".allChoices").on("click", "p", function(){
         var userAnswer = $(this).text();
+        console.log(userAnswer);
         var correctAnswerIndex = quiz[currentQuestionIndex].answer;
         var correctAnswer = quiz[currentQuestionIndex].choices[correctAnswerIndex];
+console.log(correctAnswer);
+
+// when user click on the right answer color: green else red.
         if (userAnswer === correctAnswer) {
-          alert("YOU GOT IT!");
+            $('p').css ('background-color', 'green');
         } else {
-            alert("WRONG!");
-          }
+            $('p').css ('background-color', '#fb4a68');
+        }
+    });
+
+
+    $('#newGameButton').click(function(){
+        console.log('yes start again');
+
+
     });
 
 });
 
 
 
-
+// make an array newGame that allow user to start a new game
 
 // Make an array
 //Should have objects within the array
